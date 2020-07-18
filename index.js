772048bf2d33e4594afb1ed1204994ab5ec431db
blackdoglu <68472153+blackdoglu@users.noreply.github.com>
2020-07-19 00:00:00 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+
+	var db = req.con;
+    var data = "";
+
+    db.query('SELECT * FROM account', function(err, rows) {
+        if (err) {
+            return console.error('error: ' + err.message);
+        }
+        var data = rows;
+
+        // use index.ejs
+        res.render('index', { title: 'Account Information', data: data});
+    });
+
+});
+module.exports = router;
